ea31ca052c73b29fcfb7e21346162ddc81da92b0
enautirakasle <enaut.irakasle@gmail.com>
2021-12-04 23:51:32 +0100

    jokutxoa eginda

--- main.ts
+++ main.ts
@@ -1,4 +1,27 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.No)
+input.onButtonPressed(Button.A, function () {
+    sprite.move(-1)
 })
-basic.showIcon(IconNames.Heart)
+input.onButtonPressed(Button.B, function () {
+    sprite.move(1)
+})
+let sprite: game.LedSprite = null
+let velocidad = 1000000
+game.setScore(0)
+sprite = game.createSprite(2, 4)
+let tanta = game.createSprite(randint(0, 4), 0)
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        control.waitMicros(velocidad)
+        tanta.change(LedSpriteProperty.Y, 1)
+    }
+    if (sprite.isTouching(tanta)) {
+        game.addScore(1)
+        velocidad += -95000
+    }
+    if (10 == game.score()) {
+        game.gameOver()
+    }
+    control.waitMicros(1000000)
+    tanta.delete()
+    tanta = game.createSprite(randint(0, 4), 0)
+})
